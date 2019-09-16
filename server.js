1391368a88555ce1a59232f365eef496fa5cdc3a
habernet <phaberern@gmail.com>
2019-09-16 19:54:23 -0400

    Route files added...only for testing

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ connectDB();
 //Routes
 app.get("/", (req, res) => res.send("API Running"));
 
+//Define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+
 // Define port based on deployment
 const PORT = process.env.PORT || 5000;
 
