5967a4c69eae0c8970aa743a279d979695ed33e7
habernet <phaberern@gmail.com>
2019-09-16 16:02:21 -0400

    Mongo connection is live

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
+//Bring in express
 const express = require("express");
-
+//Mongo DB
+const connectDB = require("./config/db");
+//Initialize express app
 const app = express();
+//Connect to the DB
+connectDB();
 
+//Routes
 app.get("/", (req, res) => res.send("API Running"));
 
+// Define port based on deployment
 const PORT = process.env.PORT || 5000;
 
+//Kick off the server
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}!`);
 });
