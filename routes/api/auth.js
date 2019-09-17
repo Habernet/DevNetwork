82b45ce4eced41b2ea17c7ac0d97972ec197d964
habernet <phaberern@gmail.com>
2019-09-17 17:17:23 -0400

    User auth fully working...can protect routes and auth users

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,7 +3,12 @@ const router = express.Router();
 // Bring in our custom middleware
 const auth = require("../../middleware/auth");
 // Bring in the user model
-const Usedr = require("../../models/User");
+const User = require("../../models/User");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const bcrypt = require("bcryptjs");
+//Bring in the middleware to validate the req.body;
+const { check, validationResult } = require("express-validator");
 
 // @route GET api/auth
 // @desc Test route
@@ -16,9 +21,76 @@ router.get("/", auth, async (req, res) => {
     res.json(user);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send("Server Error");
+    res.status(500).send("Server Error for GET to /api/auth");
   }
 });
+
+// @route POST api/auth
+// @desc authenticate the user and then get the token
+// @access Public
+router.post(
+  "/",
+  [
+    check("email", "Please include a valid email.").isEmail(),
+    check("password", "Password required.").exists()
+  ],
+  async (req, res) => {
+    //Get your errors
+    const errors = validationResult(req);
+    //If they exist..respond accordingly
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    //Destructure the req.body
+    const { email, password } = req.body;
+
+    try {
+      // See if user exists
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials." }] });
+      }
+
+      //User has been found so match the password, we do that with the password they entered into the req (plain text), and the encrtypted password we got back from the DB in the above query.
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials." }] });
+      }
+
+      //Create the payload for the token
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      // Sign the token and send it back to the client
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+
+      // return the json web token
+    } catch (err) {
+      console.error(err.message);
+      //   console.log(err);
+      res.status(500).send("Server Error for POST to /api/auth");
+    }
+  }
+);
 
 //Export the route
 module.exports = router;
