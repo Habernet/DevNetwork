d56572ad9156db0017a558ea25eb31f997dc211d
habernet <phaberern@gmail.com>
2019-09-25 12:04:59 -0400

    /api/profile/me route created

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,33 @@
 const express = require("express");
 const router = express.Router();
+// Bring in auth middleware for protected routes
+const auth = require("../../middleware/auth");
+//Bring in the models
+const User = require("../../models/User");
+const Profile = require("../../models/Profile");
 
-// @route GET api/profile
-// @desc Test route
-// @access Public
-router.get("/", (req, res) => res.send("Profile route"));
+// @route GET api/profile/me
+// @desc Get current users profile by the user id in the token
+// @access Private
+router.get("/me", auth, async (req, res) => {
+  try {
+    // Get the profile based on the token
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      "user",
+      ["name", "avatar"]
+    );
 
-//Export the route
+    if (!profile) {
+      return res
+        .status(400)
+        .json({ msg: "There is no profile for this user." });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Sever Error");
+  }
+});
+
+//Export the router
 module.exports = router;
