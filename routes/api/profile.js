3cd353a10f39d0ccf2a1c85660a09440629aad65
habernet <phaberern@gmail.com>
2019-09-26 10:37:51 -0400

    route for get all user profiles complete

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -103,5 +103,19 @@ router.post(
   }
 );
 
+// @route    GET api/profile
+// @desc     Get all profiles
+// @access   Public
+
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    res.json(profiles);
+  } catch (err) {
+    console.err(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 //Export the router
 module.exports = router;
