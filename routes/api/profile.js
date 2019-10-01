1eeaa5509baba08d57ac8178c68b1bc8c095e60c
habernet <phaberern@gmail.com>
2019-10-01 17:47:45 -0400

    Delete experiences route complete

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -217,5 +217,26 @@ router.put(
   }
 );
 
+// @route    DELETE api/profile/experience/:exp_id
+// @desc     Delete experience from profile
+// @access   Private
+router.delete("/experience/:exp_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    // Get the remove index and splice it off and then save the user profile
+    const removeIndex = profile.experience
+      .map(item => item.id)
+      .indexOf(req.params.exp_id);
+    console.log("Removing this index: ", removeIndex);
+    profile.experience.splice(removeIndex, 1);
+    await profile.save();
+
+    res.json(profile);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error.");
+  }
+});
+
 //Export the router
 module.exports = router;
